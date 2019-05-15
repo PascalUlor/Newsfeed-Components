eb2c51ad846a39358ed2d26d7957c64dcd8baa88
PascalUlor <pascalulor@yahoo.com>
2019-05-15 13:06:21 +0100

    ft(create toggle menu function) add event handler that calls toggle function

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,18 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  // Toggle the "menu--open" class on your menu refence.
+  if(![...menu.classList].includes('menu--open')) {
+    menu.classList.add('menu--open')
+    console.log(menu.classList);
+  }else if([...menu.classList].includes('menu--open')) {
+    menu.classList.remove('menu--open')
+    console.log(menu.classList);
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', toggleMenu);
